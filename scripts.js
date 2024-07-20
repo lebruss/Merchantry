2a9ecf334f3b20f86b05c1b59d1f693a1ec503ff
Caleb James Russell <69431631+lebruss@users.noreply.github.com>
2024-07-21 00:10:49 +0300

    Add files via upload
    Forgot to add the JavaScript (I am a TypeScript noob.)

--- scripts.js
+++ scripts.js
@@ -9,6 +9,13 @@ var character = {
         gunpowder: 0,
         candles: 0
     },
+    lastBoughtPrices: {
+        flour: 0,
+        meat: 0,
+        sugar: 0,
+        gunpowder: 0,
+        candles: 0
+    },
     days: 0
 };
 var prices = {
@@ -35,15 +42,14 @@ function startGame() {
 }
 function updateUI() {
     document.getElementById('location').innerText = character.location;
-    document.getElementById('money').innerText = character.money.toString();
-    document.getElementById('inventory').innerText = JSON.stringify(character.inventory, null, 2);
+    document.getElementById('money').innerText = "".concat(character.money, " \u20AC");
     document.getElementById('days').innerText = character.days.toString();
     var goodsContainer = document.getElementById('goods');
     goodsContainer.innerHTML = '';
     for (var good in prices) {
         var goodElement = document.createElement('div');
         goodElement.classList.add('good');
-        goodElement.innerHTML = "\n            <span>".concat(good, ": ").concat(prices[good], " gold</span>\n            <button onclick=\"buyGood('").concat(good, "')\">Buy</button>\n            <button onclick=\"sellGood('").concat(good, "')\">Sell</button>\n        ");
+        goodElement.innerHTML = "\n            <span>".concat(good, ": ").concat(prices[good], " \u20AC (Owned: ").concat(character.inventory[good], ") (Last bought for: ").concat(character.lastBoughtPrices[good], " \u20AC)</span>\n            <div class=\"buttons\">\n                <button onclick=\"buyGood('").concat(good, "')\">Buy</button>\n                <button onclick=\"buyMaxGood('").concat(good, "')\">Buy Max</button>\n                <button onclick=\"sellGood('").concat(good, "')\">Sell</button>\n                <button onclick=\"sellAllGood('").concat(good, "')\">Sell All</button>\n            </div>\n        ");
         goodsContainer.appendChild(goodElement);
     }
 }
@@ -62,9 +68,24 @@ function randomizePrices() {
     updateUI();
 }
 function buyGood(good) {
-    if (character.money >= prices[good]) {
-        character.money -= prices[good];
+    var price = prices[good];
+    if (character.money >= price) {
+        character.money -= price;
         character.inventory[good] += 1;
+        character.lastBoughtPrices[good] = price;
+        updateUI();
+    }
+    else {
+        showAlert('Not enough money!');
+    }
+}
+function buyMaxGood(good) {
+    var price = prices[good];
+    var maxBuyable = Math.floor(character.money / price);
+    if (maxBuyable > 0) {
+        character.money -= maxBuyable * price;
+        character.inventory[good] += maxBuyable;
+        character.lastBoughtPrices[good] = price;
         updateUI();
     }
     else {
@@ -73,8 +94,21 @@ function buyGood(good) {
 }
 function sellGood(good) {
     if (character.inventory[good] > 0) {
-        character.money += prices[good];
+        var price = prices[good];
+        character.money += price;
         character.inventory[good] -= 1;
+        updateUI();
+    }
+    else {
+        showAlert('No inventory to sell!');
+    }
+}
+function sellAllGood(good) {
+    var quantity = character.inventory[good];
+    if (quantity > 0) {
+        var price = prices[good];
+        character.money += quantity * price;
+        character.inventory[good] = 0;
         updateUI();
     }
     else {
